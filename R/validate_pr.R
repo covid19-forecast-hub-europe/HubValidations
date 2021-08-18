bad709cba586f59c44b6acca0ddb574b81b3b94a
Hugo Gruson <hugo.gruson@protonmail.com>
2021-08-18 17:41:14 +0200

    Create validate_pr() function

--- R/validate_pr.R
+++ R/validate_pr.R
@@ -1,22 +1,29 @@
 #' @examples
 #' validate_pr(
 #'   "epiforecasts/covid19-forecast-hub-europe",
-#'   "536"
+#'   536,
+#'   data_folder = "data-processed"
 #' )
 
-validate_pr <- function(gh_repo, pr_number) {
+validate_pr <- function(gh_repo, pr_number, data_folder, ...) {
 
   pr <- gh::gh(paste("/repos", gh_repo, "pulls", pr_number, sep = "/"))
 
   pr_head <- pr$head
 
-  tmp <- fs::dir_create(paste0(tempdir(), "/", pr_head$user, "_", pr_head$repo$name))
+  tmp <- paste0(tempdir(), "/", pr_head$user$login, "_", pr_head$repo$name)
 
-  gert::git_clone(
-    url = pr_head$repo$html_url,
-    branch = pr_head$ref,
-    path= tmp
-  )
+  if (!fs::dir_exists(tmp)) {
+    fs::dir_create(tmp)
+    gert::git_clone(
+      url = pr_head$repo$html_url,
+      branch = pr_head$ref,
+      path = tmp
+    )
+  }
+
+  validate_repository(fs::path(tmp, data_folder), ...)
+
 
 }
 
