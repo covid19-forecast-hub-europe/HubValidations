87e020e8272b0128e585a0c2fa7b8c493afc28b3
Hugo Gruson <hugo.gruson@protonmail.com>
2021-09-01 17:30:50 +0200

    Add test for print method

--- tests/testthat/test-S3_fhub_validations.R
+++ tests/testthat/test-S3_fhub_validations.R
@@ -1,3 +1,13 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("print method", {
+
+  res <- expect_silent({
+    validate_model_forecast(
+      system.file("testdata", "example-model", "2021-07-26-example-model.csv",
+                  package = "ForecastHubValidations"),
+      system.file("testdata", "forecast-schema.yml",
+                  package = "ForecastHubValidations")
+    )})
+
+  expect_output(print(res))
+
 })
