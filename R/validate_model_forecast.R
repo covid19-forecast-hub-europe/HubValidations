43e0ac277cd1e5aa298a7b2e2e9353b157863c6b
Hugo Gruson <hugo.gruson@protonmail.com>
2021-09-06 16:06:07 +0200

    Fix indent style

--- R/validate_model_forecast.R
+++ R/validate_model_forecast.R
@@ -13,12 +13,12 @@
 #'
 #' @examples
 #' validate_model_forecast(
-#'   system.file("
-#'     testdata", "example-model", "2021-07-26-example-model.csv",
+#'   system.file(
+#'     "testdata", "example-model", "2021-07-26-example-model.csv",
 #'     package = "ForecastHubValidations"
 #'   ),
-#'   system.file("
-#'     testdata", "forecast-schema.yml",
+#'   system.file(
+#'     "testdata", "forecast-schema.yml",
 #'     package = "ForecastHubValidations"
 #'   )
 #' )
