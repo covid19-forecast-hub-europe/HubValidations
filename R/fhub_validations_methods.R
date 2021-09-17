40258198efefbefceb63a003bda85c138f224978
Hugo Gruson <hugo.gruson@protonmail.com>
2021-09-17 11:32:24 +0200

    Remove S3 system and add docs

--- R/fhub_validations_methods.R
+++ R/fhub_validations_methods.R
@@ -36,17 +36,20 @@ summary.fhub_validations <- function(x, ...) {
 
 }
 
+#' Raise conditions stored in a `fhub_validations` object
+#'
+#' This is meant to be used in CI workflows to raise conditions from
+#' `fhub_validations` objects.
+#'
+#' @param x A `fhub_validations` object
+#'
+#' @return An error if one of the elements of `x` is of class `fhub_failure` or
+#' `fhub_unrecoverable_error`. `TRUE` invisibly otherwise.
+#'
 #' @export
-check_for_errors <- function(x, ...) {
-  UseMethod("report")
-}
-
-
-#' @export
-check_for_errors.fhub_validations <- function(x, ...) {
-
+check_for_errors <- function(x) {
   failures <- x[map_lgl(x, ~ inherits_any(.x, "fhub_failure"))]
-  errors <- x[map_lgl(x, ~ inherits_any(.x, "fhub_failure"))]
+  errors <- x[map_lgl(x, ~ inherits_any(.x, "fhub_unrecoverable_error"))]
 
   pb <- c(failures, errors)
   class(pb) <- c("fhub_validations", "list")
@@ -59,5 +62,6 @@ check_for_errors.fhub_validations <- function(x, ...) {
     )
   }
 
-  return(TRUE)
+  return(invisible(TRUE))
 }
+
